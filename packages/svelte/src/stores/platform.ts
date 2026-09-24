dbeee7e598dbbf45adbadcdd730e5d252d22a8c6
glennmichael123 <gtorregosa@gmail.com>
2026-09-24 19:36:39 +0800

    fix(svelte): expose public store declaration types

--- packages/svelte/src/stores/platform.ts
+++ packages/svelte/src/stores/platform.ts
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store';
+import type { Readable } from 'svelte/store';
 import { craft } from './craft';
 
-export function createPlatformStore() {
+export function createPlatformStore(): Readable<{ platform: string; version: string } | null> {
   const { subscribe, set } = writable<{ platform: string; version: string } | null>(null);
 
   craft.subscribe(async (_$craft) => {
